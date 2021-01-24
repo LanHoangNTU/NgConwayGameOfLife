2e7b1fa53d0b7b0eca311ddec1a2e401105385cd
LanHoangNTU <lanhoang1718@gmail.com>
2021-01-24 11:13:01 +0800

    Add Controls and Canvas components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CanvasComponent
+    CanvasComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
